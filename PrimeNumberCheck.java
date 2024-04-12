460a55600e3c79043060230c45c021dee0c95d74
Thameem-20 <ahmedthameem20@gmail.com>
2024-04-12 13:00:00 +0530

    updated

--- PrimeNumberCheck.java
+++ PrimeNumberCheck.java
@@ -12,7 +12,10 @@ public class PrimeNumberCheck {
 		if (n == 2) {
 			return true;
 		}
-		
+		for (int i = 2; i <= n / 2; i++) {
+			if (n % i == 0) {
+				return false;
+			}
 		}
 
 		return true;
