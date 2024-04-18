75a04857bf1aa183f3e10eea7dba9634dbc55916
Thameem-20 <ahmedthameem20@gmail.com>
2024-04-18 13:00:00 +0530

    updated

--- MergeSort.java
+++ MergeSort.java
@@ -1,7 +1,7 @@
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] arr = { };
+		int[] arr = { 70, 50, 30, 10, 20, 40, 60 };
 
 		int[] merged = mergeSort(arr, 0, arr.length - 1);
 
